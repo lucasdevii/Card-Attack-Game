9338f43d8ab39a6a0b121145ab0f9b87125cad06
Lucas <lucassousa180810@gmail.com>
2026-03-30 23:46:41 -0300

    feat: estilização dos cards completa por enquanto

--- front/src/components/gameTable/Card.vue
+++ front/src/components/gameTable/Card.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 })
 </script>
 <template>
-        <div class="bg-gray-900 w-[250px] h-[430px] p-3 rounded-2xl space-y-2">
+        <div class="bg-gray-900 w-[240px] h-[400px] p-3 rounded-2xl space-y-2 hover:scale-105 transition-transform cursor-pointer">
             <!-- Imagem -->
             <div class="bg-black w-full h-2/5 flex justify-center items-center">
                 <img :src="image" alt="" v-if="image">
@@ -22,8 +22,6 @@ const props = defineProps({
             <div class="h-3/5">
                 <!-- Descrição -->
                 <div class="flex flex-col h-5/6">
-                    <h1>Descrição</h1>
-
                     <div class="flex-1 border rounded-lg p-1 overflow-auto">
                         <p class="break-words text-xs">
                         Faz tal coisa e bla bla, mas tambem faz isso e aquilo lá que é bem bacana para que o mundo continue sendo bom e ruim ao mesmo tempo fora dessa realidade material demiurgeal onde nós sofremos em um loop incessante de dor e sofrimento assim fazendo com que o mundo 
--- front/src/views/GameTable.vue
+++ front/src/views/GameTable.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Card from '@/components/gameTable/Card.vue';
+import { ref } from 'vue';
+const currentCards = ref([[],[],[]]) 
 </script>
 <template>
-    <div class="w-screen h-screen relative">
-        <div class="absolute bottom-0 w-full flex justify-center space-x-4">
-            <Card/>
-            <Card/>
-            <Card/>
+    <div class="w-screen h-screen relative overflow-y-hidden">
+        <div class="absolute bottom-[-150px] w-full flex justify-center space-x-4">
+            <div @click="selectCard(index)" v-for="(card, index) in currentCards" :key="index">
+               <Card/>
+            </div>
+            
         </div>
     </div>
 </template>
