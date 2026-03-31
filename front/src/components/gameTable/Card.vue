5a77e63343d699b6d12d17b022a71397a238cabf
Lucas <lucassousa180810@gmail.com>
2026-03-30 22:43:24 -0300

    feat: Criação do component Card para as cartas, falta estilização

---
+++ front/src/components/gameTable/Card.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+        <div class="bg-gray-900 w-[250px] h-[430px] p-3">
+            <div class="bg-black w-full h-[180px]"></div>
+        </div>
+</template>
--- front/src/views/GameTable.vue
+++ front/src/views/GameTable.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import Card from '@/components/gameTable/Card.vue';
+</script>
 <template>
-    <p>asads</p>
+    <div class="w-screen h-screen relative">
+        <div class="absolute bottom-0 w-full flex justify-center space-x-4">
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    </div>
 </template>
