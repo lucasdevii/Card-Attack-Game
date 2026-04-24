2a98accba6ac710d45e101d2110773bdf765d140
Lucas <lucassousa180810@gmail.com>
2026-04-23 21:56:34 -0300

    fix: corrigido alguns bugs que aconteciam na side-bar

--- front/src/components/ui/SideBar.vue
+++ front/src/components/ui/SideBar.vue
@@ -9,17 +9,30 @@ import Icon from './Icon.vue';
 const isExpanded = ref(true);
 const isDarkMode = ref(true);
 
+const sections = {
+    'game' : 1,
+    'decks' : 2,
+    'community' : 3,
+    'config' : 4
+}
+
+const currentSection = ref(sections.game);
+
 const toggleSidebar = () => {
-  isExpanded.value = !isExpanded.value;
+    isExpanded.value = !isExpanded.value;
 };
 
 const toggleDarkMode = () => {
-  isDarkMode.value = !isDarkMode.value;
+    isDarkMode.value = !isDarkMode.value;
 };
+
+const changeSection = (section) => {
+    currentSection.value = section
+}
 
 </script>
 <template>
-    <div :class="isExpanded ? 'w-60' : 'w-15'" class="fixed left-0 top-0 h-full bg-slate-950 text-slate-100 shadow-2xl shadow-slate-950/30 transition-all duration-300">
+    <div :class="isExpanded ? 'w-60' : 'w-24'" class="fixed left-0 top-0 h-full bg-slate-950 text-slate-100 shadow-2xl shadow-slate-950/30 transition-all duration-300">
         <div class="relative flex h-full flex-col justify-between">
             <button @click="toggleSidebar" class="absolute -right-4 top-1/2 flex h-10 w-10 items-center justify-center rounded-full bg-slate-800 shadow-lg shadow-black/20 transition-colors hover:bg-slate-700">
                 <Icon :icon="isExpanded ? faChevronLeft : faChevronRight" class="h-4 w-4" />
@@ -40,24 +53,24 @@ const toggleDarkMode = () => {
 
 
                 <div class="space-y-3">
-                    <ButtonWithIcon text="Jogar" :icon="faGamepad" :collapsed="!isExpanded" active />
-                    <ButtonWithIcon text="Baralhos" :icon="CardsDeck" :collapsed="!isExpanded" />
-                    <ButtonWithIcon text="Comunidade" :icon="faUserGroup" :collapsed="!isExpanded" />
+                    <ButtonWithIcon text="Jogar" :icon="faGamepad" :collapsed="!isExpanded" :isActive="currentSection == sections.game" @click="changeSection(sections.game)"/>
+                    <ButtonWithIcon text="Cartas" :icon="CardsDeck" :collapsed="!isExpanded" :isActive="currentSection == sections.decks" @click="changeSection(sections.decks)"/>
+                    <ButtonWithIcon text="Comunidade" :icon="faUserGroup" :collapsed="!isExpanded" :isActive="currentSection == sections.community" @click="changeSection(sections.community)"/>
                 </div>
             </div>
 
             <div class="space-y-4 px-4 pb-6">
-                <ButtonWithIcon text="Configurações" :icon="faGear" :collapsed="!isExpanded" />
+                <ButtonWithIcon text="Configurações" :icon="faGear" :collapsed="!isExpanded" :isActive="currentSection == sections.config" @click="changeSection(sections.config)"/>
 
 
                     <div class="flex items-center gap-3">
-                        <div class="flex h-11 w-11 items-center justify-center rounded-3xl bg-slate-800">
+                        <div class="flex h-11 items-center justify-center rounded-3xl bg-slate-800" :class="isExpanded? 'w-11':'w-full'">
                             <Icon :icon="faMoon" class="h-4 w-4 text-slate-300" />
                         </div>
                         <div v-if="isExpanded" class="flex-1">
                             <p class="text-sm font-semibold">Modo escuro</p>
                         </div>
-                        <button @click="toggleDarkMode" class="relative h-7 w-14 rounded-full bg-slate-800 transition-colors" :class="isDarkMode ? 'bg-indigo-500' : 'bg-slate-700'" v-if="isExpanded">
+                        <button  v-if="isExpanded" @click="toggleDarkMode" class="relative h-7 w-14 rounded-full bg-slate-800 transition-colors" :class="isDarkMode ? 'bg-indigo-500' : 'bg-slate-700'">
                             <span class="absolute left-0 top-0 h-7 w-7 rounded-full bg-white shadow-lg transition-transform" :class="isDarkMode ? 'translate-x-7' : 'translate-x-0'"></span>
                         </button>
                     </div>
--- front/src/components/ui/form/ButtonWithIcon.vue
+++ front/src/components/ui/form/ButtonWithIcon.vue
@@ -2,18 +2,18 @@
 import Icon from '../Icon.vue';
 
 const emit = defineEmits(['update:value'])
-const props = defineProps({text: String, icon: Object, collapsed: Boolean, active: Boolean})
+const props = defineProps({text: String, icon: Object, collapsed: Boolean, isActive: Boolean})
 </script>
 <template>
     <div :class="[
             'h-12 w-full rounded-3xl flex items-center overflow-hidden transition-all duration-300 cursor-pointer',
-            collapsed ? 'justify-center px-2 bg-slate-800' : 'justify-start px-4 bg-slate-900',
-            props.active ? 'bg-indigo-500 text-white shadow-lg shadow-indigo-500/20' : 'text-slate-100 hover:bg-slate-800'
+            collapsed ? 'justify-center px-2 ' : 'justify-start px-4 ',
+            props.isActive ? 'bg-indigo-500 text-white shadow-lg shadow-indigo-500/20' : 'bg-slate-900 text-slate-100 hover:bg-slate-800'
         ]">
         <div class="flex items-center justify-center flex-shrink-0">
             <Icon :icon="props.icon" class="h-5 w-5"/>
         </div>
-        <span class="overflow-hidden whitespace-nowrap text-sm font-medium transition-all duration-300"
+        <span class="whitespace-nowrap text-sm font-medium transition-all duration-300"
               :class="collapsed ? 'max-w-0 opacity-0 ml-0' : 'max-w-[180px] opacity-100 ml-3'">
             {{ props.text }}
         </span>
